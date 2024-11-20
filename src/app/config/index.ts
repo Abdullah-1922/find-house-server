224cd13023962da78e5a62a835ac32fb0804a473
Md Rijwan Jannat <rijwanjannat36@gmail.com>
2024-11-20 16:58:17 +0600

    admin seeding complete

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -12,4 +12,10 @@ export default {
   jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
   jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
   bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUND,
+  admin_email: process.env.ADMIN_EMAIL,
+  admin_password: process.env.ADMIN_PASSWORD,
+  admin_first_name: process.env.ADMIN_FIRST_NAME,
+  admin_second_name: process.env.ADMIN_SECOND_NAME,
+  admin_provider: process.env.ADMIN_PROVIDER,
+  admin_image: process.env.ADMIN_IMAGE,
 };
---
+++ src/app/utils/seedAdmin.ts
@@ -0,0 +1,64 @@
+import { startSession } from "mongoose";
+import config from "../config";
+import Auth from "../modules/Auth/auth.model";
+import { User } from "../modules/User/user.model";
+import { USER_ROLE } from "../modules/User/user.utils";
+
+export const adminSeed = async () => {
+  const session = await startSession();
+
+  try {
+    session.startTransaction();
+
+    const admin = await User.findOne(
+      {
+        role: USER_ROLE.admin,
+        email: config.admin_email,
+      },
+      null,
+      { session },
+    );
+
+    if (!admin) {
+      const newAuth = await Auth.create(
+        [
+          {
+            email: config.admin_email,
+            password: config.admin_password,
+            provider: config.admin_provider,
+            role: USER_ROLE.admin,
+          },
+        ],
+        { session },
+      );
+
+      await User.create(
+        [
+          {
+            auth: newAuth[0]._id,
+            firstName: config.admin_first_name,
+            secondName: config.admin_second_name,
+            image: config.admin_image,
+            role: USER_ROLE.admin,
+            paymentHistory: [],
+            property: [],
+          },
+        ],
+        { session },
+      );
+
+      console.log("Admin created successfully...");
+    } else {
+      console.log("Admin already exists, skipping seeding...");
+    }
+
+    // Commit the transaction
+    await session.commitTransaction();
+    console.log("Seeding completed...");
+  } catch (error) {
+    await session.abortTransaction();
+    console.error("Error in seeding:", error);
+  } finally {
+    session.endSession();
+  }
+};
